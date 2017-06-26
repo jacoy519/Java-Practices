f1272b22c17486e465d015c3ba88fec730e5f763
jacoy519 <jacoy519@gmail.com>
2017-06-26 23:41:04 +0800

    UPDATE

--- tomcat-6-package-war-demo/src/main/java/com/example/demo/DemoApplication.java
+++ tomcat-6-package-war-demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication extends SpringBootServletInitializer {
@@ -18,4 +20,5 @@ public class DemoApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
 }
